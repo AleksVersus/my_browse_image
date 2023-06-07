535fcfaa6000926f1055f2684d2c4b8af7fbccf6
aleksversus <aleksversus@mail.ru>
2023-06-07 23:02:17 +0500

    feat: starting changes for set of webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s(c|a)ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    }
+};
